1459fa45513c74f42ab0f3a1e806e0e3939bbde9
admin <pcfusergandhi@gmail.com>
2019-09-15 20:13:07 +0530

    First App

---
+++ demo/src/main/java/com/pcf/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.pcf.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("First Pcf Sprin Boot App");
+
+	}
+
+}
